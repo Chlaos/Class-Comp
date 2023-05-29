b0c762d2a68f23c4eda7ebe578ecd8e305b288f2
Juan Aguilare <1201cljpat@gamil.comm>
2023-05-29 03:12:27 -0400

    first commit

---
+++ JavaScript Intro Assessment/script.js
@@ -0,0 +1,13 @@
+// Declare variables
+var firstName = "Juan";
+var lastName = "Aguilar";
+var age = 20;
+
+// Fire an alert with name
+alert("My name is " + firstName + " " + lastName);
+
+// Fire an alert with age
+alert("I am " + age + " years old");
+
+// Log a message in the console
+console.log("Having so much fun in the class YIP PEE!");
